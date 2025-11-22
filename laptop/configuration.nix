2a20b7f9b923d351f950b842f9c83b263c9c9a4c
Chester Gould <chestergould@protonmail.com>
2025-11-21 16:07:40 -0800

    removed vis

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -75,7 +75,6 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    vis
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
