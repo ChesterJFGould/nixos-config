48060ed375c492e13550c095ea2629a39b174f30
Chester Gould <chestergould@protonmail.com>
2025-11-21 16:18:09 -0800

    added flakes and nix command

--- configuration.nix
+++ configuration.nix
@@ -78,6 +78,8 @@
     vis
   ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
